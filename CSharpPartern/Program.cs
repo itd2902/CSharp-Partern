301a91c95fcfc4b54644822ecfb0768f4b332cba
quang <adquang199x@gmail.com>
2021-03-12 15:17:50 +0700

    init

---
+++ CSharpPartern/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpPartern
+{
+    public class Program
+    {
+        public enum Directions
+        {
+            Up,
+            Down,
+            Right,
+            Left
+        }
+
+        public enum Orientation
+        {
+            North,
+            South,
+            East,
+            West
+        }
+        public static void Main(string[] args)
+        {
+            var direction = Directions.Right;
+            Console.WriteLine($"Map view direction is {direction}");
+            var orientation = direction switch
+            {
+                Directions.Up => Orientation.North,
+                Directions.Down => Orientation.South,
+                Directions.Left => Orientation.West,
+                Directions.Right => Orientation.East,
+                _ => throw new NotImplementedException()
+            };
+            var (_, beta) = ("a", "b");
+            Console.WriteLine(beta);
+            Point point = new Point(3, 5);
+            (double X, double Y) = point;
+            Console.WriteLine(X + "," + Y);
+            Console.ReadKey();
+
+        }
+        public class Point
+        {
+            public Point(double x, double y) => (X, Y) = (x, y);
+            public double X { get; set; }
+            public double Y { get; set; }
+            public void Deconstruct(out double x, out double y) => (x, y) = (X, Y);
+        }
+    }
+}
---
+++ TypeMatchingPartern/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TypeMatchingPartern
+{
+    class Program
+    {
+        class Rectangle
+        {
+            public Rectangle(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public int Width { get; set; }
+            public int Height { get; set; }
+        }
+
+        class Cirle
+        {
+            public int Radius { get; set; }
+        }
+
+        class Square
+        {
+            public int Length { get; set; }
+        }
+        public static double Acreage(object shape)
+            => shape switch
+            {
+                Rectangle r when r.Width > 10 && r.Height > 10 => r.Width * r.Height,
+                Cirle c => Math.PI * Math.Pow(c.Radius, 2),
+                Square q => Math.Pow(q.Length, 2),
+                _ => 0
+            };
+        static void Main(string[] args)
+        {
+            var shape = new Rectangle(12, 11);
+            Console.WriteLine(Acreage(shape));
+            Console.ReadKey();
+        }
+    }
+}
